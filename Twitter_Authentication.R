7081c9d8fa13af0be18be811a735aaba43fcd9f6
MAHESH D <maheshdosm@gmail.com>
2021-04-20 11:20:38 +0530

    Add files via upload
    Codes for the paper Public Emotion and Sentiment Analysis of Covid-19 Tweets Using R-Shiny.

---
+++ Twitter_Authentication.R
@@ -0,0 +1,14 @@
+#........ Login Information ........
+
+consumerKey <- "9AFrgPX3Jglfv6kPlMhMeNcqt"
+
+consumerSecret <- "lbXpoQLdafUIoHkAm38ZSp4YBeX6Lj9IG7p48ZlfAeRilRO7ep"
+
+accessToken <- "796400083247988741-HAQLMFabt7cvh14iePqo83eXSiO0uFX"
+
+accessTokenSecret <-  "85X2Ra8rMBjYSAEkHi8Si7a29AjV4whpznSEh9SvYbxct"
+
+#set up 
+setup_twitter_oauth(consumerKey,consumerSecret,accessToken,accessTokenSecret)
+
+
